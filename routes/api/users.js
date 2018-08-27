3da1ca82d4dd3056fd071317c049d1d1c1c24c1b
Unknown <phil.mcvay90@gmail.com>
2018-08-27 21:31:13 +0100

    Set up jwt and passport

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const passport = require('passport')
+const keys = require('../../config/keys')
 
 // Load User model
 const User = require('../../models/User')
@@ -57,12 +60,30 @@ router.post('/login', (req, res) => {
       bcrypt.compare(password, user.password)
         .then(match => {
           if (match) {
-            res.json({ msg: 'Success' })
+            const payload = { id: user.id, name: user.name, avatar: user.avatar }
+            // Sign token
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              { expiresIn: 3600 },
+              (error, token) => res.json({ success: true, token: `Bearer ${token}` })
+            )
           } else {
             return res.status(400).json({ password: 'Incorrect password' })
           }
         })
     })
+})
+
+// @route    GET api/users/current
+// @desc     Return current user
+// @access   Private
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
+  })
 })
 
 module.exports = router
