1fa02ef0c0d01d65595e3c53062a6a11a213acf8
Unknown <phil.mcvay90@gmail.com>
2018-09-28 23:53:31 +0100

    Converted getCurrentUser action to async func. Added loading delay, react-tag styling

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -2,22 +2,28 @@ import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE } from './types'
 import axios from 'axios'
 
 // Get current profile
-export const getCurrentProfile = () => dispatch => {
-  dispatch(setProfileLoading())
-  axios
-    .get('/api/profile')
-    .then(res => {
-      dispatch({
-        type: GET_PROFILE,
-        payload: res.data
+export function getCurrentProfile() {
+  const loadingDelay = delay => new Promise(resolve => { setTimeout(resolve, delay) });
+
+  return async (dispatch) => {
+    dispatch(setProfileLoading())
+    // Fake loading for 0.7 seconds before axios call
+    await loadingDelay(700)
+    await axios
+      .get('/api/profile')
+      .then(res => {
+        dispatch({
+          type: GET_PROFILE,
+          payload: res.data
+        })
       })
-    })
-    .catch(() => {
-      dispatch({
-        type: GET_PROFILE,
-        payload: {}
+      .catch(() => {
+        dispatch({
+          type: GET_PROFILE,
+          payload: {}
+        })
       })
-    })
+  }
 }
 
 // Profile loading
