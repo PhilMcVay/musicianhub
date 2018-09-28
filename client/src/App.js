b056fccee63392b73fa605cd30412d202e227cf7
Unknown <phil.mcvay90@gmail.com>
2018-09-28 21:27:09 +0100

    CreateProfile component form work, adjustments to Profile model

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import Landing from './components/layout/Landing'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
 import Dashboard from './components/dashboard/Dashboard'
+import CreateProfile from './components/create-profile/CreateProfile'
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -46,6 +47,9 @@ class App extends Component {
               <Route exact path="/login" component={ Login } />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={ Dashboard } />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/create-profile" component={ CreateProfile } />
               </Switch>
             </div>
           </div>
---
+++ client/src/components/create-profile/CreateProfile.js
@@ -0,0 +1,129 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+class CreateProfile extends Component {
+  state = {
+    displaySocialInputs: false,
+    handle: '',
+    age: '',
+    gender: '',
+    location: '',
+    website: '',
+    bio: '',
+    recordingExperience: '',
+    yearsPlayedMusic: '',
+    gigsPlayed: '',
+    availableToRehearse: '',
+    availableToGig: '',
+    lookingForBand: null,
+    lookingForBandmates: null,
+    instruments: '',
+    genres: '',
+    facebook: '',
+    twitter: '',
+    youtube: '',
+    soundcloud: '',
+    bandcamp: '',
+    errors: {}
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+  }
+
+  render() {
+    const { errors } = this.state
+
+    return (
+      <div className="create-profile-container max-width-form">
+        <div className="form-container">
+          <div className="form-inner">
+            <form id="create-profile-form" action="" onSubmit={this.handleSubmit} noValidate>
+              <h1 className="form-header">Create your profile</h1>
+              <small>* = required fields</small>
+              <div className="form-body">
+                <div>
+                  <label>* Handle</label>
+                  <input
+                    onChange={this.handleChange}
+                    type="text"
+                    value={this.state.handle}
+                    name="handle"
+                    autoComplete="off"
+                  />
+                </div>
+                <div>
+                  <label>Age</label>
+                  <input
+                    onChange={this.handleChange}
+                    type="text"
+                    value={this.state.age}
+                    name="age"
+                    autoComplete="off"
+                  />
+                </div>
+                <div>
+                  <label>Gender</label>
+                  <select className="form-select" value={this.state.gender} onChange={this.handleChange} name="gender">
+                    <option value=""></option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                  </select>
+                </div>
+                <div>
+                  <label>Location</label>
+                  <input
+                    onChange={this.handleChange}
+                    type="text"
+                    value={this.state.location}
+                    name="location"
+                    autoComplete="off"
+                  />
+                </div>
+                <div className="grid-full-width">
+                  <label>Instruments</label>
+                  <input
+                    onChange={this.handleChange}
+                    type="text"
+                    value={this.state.instruments}
+                    name="instruments"
+                    autoComplete="off"
+                  />
+                </div>
+                <div className="grid-full-width">
+                  <label>Genres</label>
+                  <input
+                    onChange={this.handleChange}
+                    type="text"
+                    value={this.state.genres}
+                    name="genres"
+                    autoComplete="off"
+                  />
+                </div>
+              </div>
+              <button className="button button-darkblue" type="submit">Create</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+CreateProfile.propTypes = {
+  profile: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+  errors: state.errors
+})
+
+export default connect(mapStateToProps)(CreateProfile)
