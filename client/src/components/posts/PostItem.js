99e7f81171060546d6ead234385d2a0057bfdecc
Unknown <phil.mcvay90@gmail.com>
2018-11-14 23:10:41 +0000

    Implemented deletePost, addLike, removeLike for posts

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { deletePost, addLike, removeLike } from '../../actions/postActions'
 import moment from 'moment'
 import '../../styles/components/PostItem.css'
 
 class PostItem extends Component {
   handleDeletePost = (id) => {
-    console.log(id)
+    this.props.deletePost(id)
+  }
+
+  handleAddLike = (id) => {
+    this.props.addLike(id)
+  }
+
+  handleRemoveLike = (id) => {
+    this.props.removeLike(id)
   }
 
   render() {
@@ -20,15 +29,15 @@ class PostItem extends Component {
           <p className="post-time">{moment(date).startOf('minute').fromNow()}</p>
           <p>{text}</p>
           <div className="postactions-container">
-            <button type="button">
+            <button type="button" onClick={() => { this.handleAddLike(_id) }}>
               <i class="far fa-thumbs-up"><span className="like-counter">{likes.length}</span></i>
             </button>
-            <button type="button">
+            <button type="button" onClick={() => { this.handleRemoveLike(_id) }}>
               <i class="far fa-thumbs-down"></i>
             </button>
             {
               this.props.post.user === auth.user.id &&
-              <button type="button" className="delete-post-button" onClick={this.handleDeletePost(_id)}>
+              <button type="button" className="delete-post-button" onClick={() => { this.handleDeletePost(_id) }}>
                 <i class="fas fa-times"></i>
               </button>
             }
@@ -41,11 +50,14 @@ class PostItem extends Component {
 
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  deletePost: PropTypes.func.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired
 }
 
 const mapStateToProps = state => ({
   auth: state.auth,
 })
 
-export default connect(mapStateToProps)(PostItem)
+export default connect(mapStateToProps, { deletePost, addLike, removeLike })(PostItem)
--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,4 +1,4 @@
-import { ADD_POST, GET_POSTS } from '../actions/types'
+import { ADD_POST, GET_POSTS, DELETE_POST } from '../actions/types'
 
 const initialState = {
   posts: []
@@ -15,6 +15,11 @@ export default function(state = initialState, action) {
       return {
         ...state,
         posts: action.payload
+      }
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter(post => post._id !== action.payload)
       }
     default:
       return state
