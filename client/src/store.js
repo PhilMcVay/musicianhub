4ceb96a3f4f74aa549ddb8539b6e0bf401f56a46
Unknown <phil.mcvay90@gmail.com>
2018-11-16 19:47:05 +0000

    Fixed redux devtools issue when not installed on device/machine

--- client/src/store.js
+++ client/src/store.js
@@ -5,13 +5,20 @@ import rootReducer from './reducers'
 const initialState = {}
 const middleware = [thunk]
 
+const composeEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer =
+  composeEnhancers(
+    applyMiddleware(...middleware),
+  );
+
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  enhancer
 )
 
 export default store
