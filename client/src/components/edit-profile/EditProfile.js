bdfecf946102c78fcd45101ad8b7e6fc1389b14a
pmcvay <phil.mcvay90@gmail.com>
2020-09-07 20:22:07 +0100

    Changed required icon color to red

--- client/src/components/edit-profile/EditProfile.js
+++ client/src/components/edit-profile/EditProfile.js
@@ -1,69 +1,95 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import TagsInput from 'react-tagsinput'
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
-import { createProfile, getCurrentProfile } from '../../actions/profileActions'
-import isEmpty from '../../validation/isEmpty'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import TagsInput from "react-tagsinput";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { createProfile, getCurrentProfile } from "../../actions/profileActions";
+import isEmpty from "../../validation/isEmpty";
 
 class EditProfile extends Component {
   state = {
     displaySocialInputs: false,
-    handle: '',
-    age: '',
-    gender: '',
-    location: '',
-    bio: '',
-    yearsPlayedMusic: '',
-    gigsPlayed: '',
-    availableToRehearse: '',
-    availableToGig: '',
+    handle: "",
+    age: "",
+    gender: "",
+    location: "",
+    bio: "",
+    yearsPlayedMusic: "",
+    gigsPlayed: "",
+    availableToRehearse: "",
+    availableToGig: "",
     recordingExperience: false,
     lookingForBand: false,
     lookingForBandmates: false,
     instruments: [],
     genres: [],
-    website: '',
-    facebook: '',
-    twitter: '',
-    youtube: '',
-    soundcloud: '',
-    bandcamp: '',
-    errors: {}
-  }
+    website: "",
+    facebook: "",
+    twitter: "",
+    youtube: "",
+    soundcloud: "",
+    bandcamp: "",
+    errors: {},
+  };
 
   componentDidMount() {
-    this.props.getCurrentProfile()
+    this.props.getCurrentProfile();
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.errors) {
-      this.setState({ errors: nextProps.errors })
+      this.setState({ errors: nextProps.errors });
     }
     if (nextProps.profile.profile) {
-      const profile = nextProps.profile.profile
+      const profile = nextProps.profile.profile;
 
       // If profile field doesn't exist, make empty string
-      profile.age = !isEmpty(profile.age) ? profile.age : ''
-      profile.gender = !isEmpty(profile.gender) ? profile.gender : ''
-      profile.location = !isEmpty(profile.location) ? profile.location : ''
-      profile.bio = !isEmpty(profile.bio) ? profile.bio : ''
-      profile.yearsPlayedMusic = !isEmpty(profile.yearsPlayedMusic) ? profile.yearsPlayedMusic : ''
-      profile.gigsPlayed = !isEmpty(profile.gigsPlayed) ? profile.gigsPlayed : ''
-      profile.availableToRehearse = !isEmpty(profile.availableToRehearse) ? profile.availableToRehearse : ''
-      profile.availableToGig = !isEmpty(profile.availableToGig) ? profile.availableToGig : ''
-      profile.recordingExperience = !isEmpty(profile.recordingExperience) ? profile.recordingExperience : false
-      profile.lookingForBand = !isEmpty(profile.lookingForBand) ? profile.lookingForBand : false
-      profile.lookingForBandmates = !isEmpty(profile.lookingForBandmates) ? profile.lookingForBandmates : false
-      profile.instruments = !isEmpty(profile.instruments) ? profile.instruments : []
-      profile.genres = !isEmpty(profile.genres) ? profile.genres : []
-      profile.website = !isEmpty(profile.website) ? profile.website : ''
-      profile.social = !isEmpty(profile.social) ? profile.social : {}
-      profile.facebook = !isEmpty(profile.social.facebook) ? profile.social.facebook : ''
-      profile.twitter = !isEmpty(profile.social.twitter) ? profile.social.twitter : ''
-      profile.youtube = !isEmpty(profile.social.youtube) ? profile.social.youtube : ''
-      profile.soundcloud = !isEmpty(profile.social.soundcloud) ? profile.social.soundcloud : ''
-      profile.bandcamp = !isEmpty(profile.social.bandcamp) ? profile.social.bandcamp : ''
+      profile.age = !isEmpty(profile.age) ? profile.age : "";
+      profile.gender = !isEmpty(profile.gender) ? profile.gender : "";
+      profile.location = !isEmpty(profile.location) ? profile.location : "";
+      profile.bio = !isEmpty(profile.bio) ? profile.bio : "";
+      profile.yearsPlayedMusic = !isEmpty(profile.yearsPlayedMusic)
+        ? profile.yearsPlayedMusic
+        : "";
+      profile.gigsPlayed = !isEmpty(profile.gigsPlayed)
+        ? profile.gigsPlayed
+        : "";
+      profile.availableToRehearse = !isEmpty(profile.availableToRehearse)
+        ? profile.availableToRehearse
+        : "";
+      profile.availableToGig = !isEmpty(profile.availableToGig)
+        ? profile.availableToGig
+        : "";
+      profile.recordingExperience = !isEmpty(profile.recordingExperience)
+        ? profile.recordingExperience
+        : false;
+      profile.lookingForBand = !isEmpty(profile.lookingForBand)
+        ? profile.lookingForBand
+        : false;
+      profile.lookingForBandmates = !isEmpty(profile.lookingForBandmates)
+        ? profile.lookingForBandmates
+        : false;
+      profile.instruments = !isEmpty(profile.instruments)
+        ? profile.instruments
+        : [];
+      profile.genres = !isEmpty(profile.genres) ? profile.genres : [];
+      profile.website = !isEmpty(profile.website) ? profile.website : "";
+      profile.social = !isEmpty(profile.social) ? profile.social : {};
+      profile.facebook = !isEmpty(profile.social.facebook)
+        ? profile.social.facebook
+        : "";
+      profile.twitter = !isEmpty(profile.social.twitter)
+        ? profile.social.twitter
+        : "";
+      profile.youtube = !isEmpty(profile.social.youtube)
+        ? profile.social.youtube
+        : "";
+      profile.soundcloud = !isEmpty(profile.social.soundcloud)
+        ? profile.social.soundcloud
+        : "";
+      profile.bandcamp = !isEmpty(profile.social.bandcamp)
+        ? profile.social.bandcamp
+        : "";
 
       // Set component state
       this.setState({
@@ -86,29 +112,29 @@ class EditProfile extends Component {
         twitter: profile.twitter,
         youtube: profile.youtube,
         soundcloud: profile.soundcloud,
-        bandcamp: profile.bandcamp
-      })
+        bandcamp: profile.bandcamp,
+      });
     }
   }
 
   handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
   handleInstrumentsTagChange = (tags) => {
-    this.setState({ instruments: tags })
-  }
+    this.setState({ instruments: tags });
+  };
 
   handleGenresTagChange = (tags) => {
-    this.setState({ genres: tags })
-  }
+    this.setState({ genres: tags });
+  };
 
   handleCheckboxChange = (e) => {
-    this.setState({ [e.target.name]: e.target.checked })
-  }
+    this.setState({ [e.target.name]: e.target.checked });
+  };
 
   handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const profileData = {
       handle: this.state.handle,
       age: this.state.age,
@@ -129,17 +155,19 @@ class EditProfile extends Component {
       twitter: this.state.twitter,
       youtube: this.state.youtube,
       soundcloud: this.state.soundcloud,
-      bandcamp: this.state.bandcamp
-    }
-    this.props.createProfile(profileData, this.props.history)
-  }
+      bandcamp: this.state.bandcamp,
+    };
+    this.props.createProfile(profileData, this.props.history);
+  };
 
   toggleSocialInputs = () => {
-    this.setState(prevState => ({ displaySocialInputs: !prevState.displaySocialInputs }))
-  }
+    this.setState((prevState) => ({
+      displaySocialInputs: !prevState.displaySocialInputs,
+    }));
+  };
 
   renderSocialInputs = () => {
-    const { errors } = this.state
+    const { errors } = this.state;
 
     return (
       <React.Fragment>
@@ -151,9 +179,13 @@ class EditProfile extends Component {
             value={this.state.website}
             name="website"
             autoComplete="off"
-            className={"social-input-website " + ( errors.website ? "input-error" : "" ) }
+            className={
+              "social-input-website " + (errors.website ? "input-error" : "")
+            }
           />
-          { errors.website && <small className="input-error-message">{ errors.website }</small> }
+          {errors.website && (
+            <small className="input-error-message">{errors.website}</small>
+          )}
         </div>
         <div>
           <label>Facebook</label>
@@ -163,9 +195,13 @@ class EditProfile extends Component {
             value={this.state.facebook}
             name="facebook"
             autoComplete="off"
-            className={"social-input-facebook " + ( errors.facebook ? "input-error" : "" ) }
+            className={
+              "social-input-facebook " + (errors.facebook ? "input-error" : "")
+            }
           />
-          { errors.facebook && <small className="input-error-message">{ errors.facebook }</small> }
+          {errors.facebook && (
+            <small className="input-error-message">{errors.facebook}</small>
+          )}
         </div>
         <div>
           <label>Twitter</label>
@@ -175,9 +211,13 @@ class EditProfile extends Component {
             value={this.state.twitter}
             name="twitter"
             autoComplete="off"
-            className={"social-input-twitter " + ( errors.twitter ? "input-error" : "" ) }
+            className={
+              "social-input-twitter " + (errors.twitter ? "input-error" : "")
+            }
           />
-          { errors.twitter && <small className="input-error-message">{ errors.twitter }</small> }
+          {errors.twitter && (
+            <small className="input-error-message">{errors.twitter}</small>
+          )}
         </div>
         <div>
           <label>YouTube</label>
@@ -187,9 +227,13 @@ class EditProfile extends Component {
             value={this.state.youtube}
             name="youtube"
             autoComplete="off"
-            className={"social-input-youtube " + ( errors.youtube ? "input-error" : "" ) }
+            className={
+              "social-input-youtube " + (errors.youtube ? "input-error" : "")
+            }
           />
-          { errors.youtube && <small className="input-error-message">{ errors.youtube }</small> }
+          {errors.youtube && (
+            <small className="input-error-message">{errors.youtube}</small>
+          )}
         </div>
         <div>
           <label>SoundCloud</label>
@@ -199,9 +243,14 @@ class EditProfile extends Component {
             value={this.state.soundcloud}
             name="soundcloud"
             autoComplete="off"
-            className={"social-input-soundcloud " + ( errors.soundcloud ? "input-error" : "" ) }
+            className={
+              "social-input-soundcloud " +
+              (errors.soundcloud ? "input-error" : "")
+            }
           />
-          { errors.soundcloud && <small className="input-error-message">{ errors.soundcloud }</small> }
+          {errors.soundcloud && (
+            <small className="input-error-message">{errors.soundcloud}</small>
+          )}
         </div>
         <div>
           <label>Bandcamp</label>
@@ -211,27 +260,38 @@ class EditProfile extends Component {
             value={this.state.bandcamp}
             name="bandcamp"
             autoComplete="off"
-            className={"social-input-bandcamp " + ( errors.bandcamp ? "input-error" : "" ) }
+            className={
+              "social-input-bandcamp " + (errors.bandcamp ? "input-error" : "")
+            }
           />
-          { errors.bandcamp && <small className="input-error-message">{ errors.bandcamp }</small> }
+          {errors.bandcamp && (
+            <small className="input-error-message">{errors.bandcamp}</small>
+          )}
         </div>
       </React.Fragment>
-    )
-  }
+    );
+  };
 
   render() {
-    const { errors, displaySocialInputs } = this.state
+    const { errors, displaySocialInputs } = this.state;
 
     return (
       <div className="create-profile-container max-width-form">
         <div className="form-container">
           <div className="form-inner">
-            <form id="create-profile-form" action="" onSubmit={this.handleSubmit} noValidate>
+            <form
+              id="create-profile-form"
+              action=""
+              onSubmit={this.handleSubmit}
+              noValidate
+            >
               <h1 className="form-header">Edit your profile</h1>
               <small>* = required fields</small>
               <div className="form-body">
                 <div>
-                  <label>Handle *</label>
+                  <label>
+                    Handle <span className="required-icon">*</span>
+                  </label>
                   <input
                     disabled
                     onChange={this.handleChange}
@@ -239,9 +299,13 @@ class EditProfile extends Component {
                     value={this.state.handle}
                     name="handle"
                     autoComplete="off"
-                    className={ errors.handle && "input-error" }
+                    className={errors.handle && "input-error"}
                   />
-                  { errors.handle && <small className="input-error-message">{ errors.handle }</small> }
+                  {errors.handle && (
+                    <small className="input-error-message">
+                      {errors.handle}
+                    </small>
+                  )}
                 </div>
                 <div>
                   <label>Age</label>
@@ -255,7 +319,12 @@ class EditProfile extends Component {
                 </div>
                 <div>
                   <label>Gender</label>
-                  <select className="form-select" value={this.state.gender} onChange={this.handleChange} name="gender">
+                  <select
+                    className="form-select"
+                    value={this.state.gender}
+                    onChange={this.handleChange}
+                    name="gender"
+                  >
                     <option value=""></option>
                     <option value="Male">Male</option>
                     <option value="Female">Female</option>
@@ -273,7 +342,12 @@ class EditProfile extends Component {
                 </div>
                 <div>
                   <label>Years Played Music</label>
-                  <select className="form-select" value={this.state.yearsPlayedMusic} onChange={this.handleChange} name="yearsPlayedMusic">
+                  <select
+                    className="form-select"
+                    value={this.state.yearsPlayedMusic}
+                    onChange={this.handleChange}
+                    name="yearsPlayedMusic"
+                  >
                     <option value=""></option>
                     <option value="1 Year">1 Year</option>
                     <option value="1-5 Years">1-5 Years</option>
@@ -283,7 +357,12 @@ class EditProfile extends Component {
                 </div>
                 <div>
                   <label>Gigs played</label>
-                  <select className="form-select" value={this.state.gigsPlayed} onChange={this.handleChange} name="gigsPlayed">
+                  <select
+                    className="form-select"
+                    value={this.state.gigsPlayed}
+                    onChange={this.handleChange}
+                    name="gigsPlayed"
+                  >
                     <option value=""></option>
                     <option value="None">None</option>
                     <option value="1-100">1-100</option>
@@ -292,7 +371,12 @@ class EditProfile extends Component {
                 </div>
                 <div>
                   <label>Available To Rehearse</label>
-                  <select className="form-select" value={this.state.availableToRehearse} onChange={this.handleChange} name="availableToRehearse">
+                  <select
+                    className="form-select"
+                    value={this.state.availableToRehearse}
+                    onChange={this.handleChange}
+                    name="availableToRehearse"
+                  >
                     <option value=""></option>
                     <option value="Once a week">Once a week</option>
                     <option value="Twice a week">Twice a week</option>
@@ -305,7 +389,12 @@ class EditProfile extends Component {
                 </div>
                 <div>
                   <label>Available To Gig</label>
-                  <select className="form-select" value={this.state.availableToGig} onChange={this.handleChange} name="availableToGig">
+                  <select
+                    className="form-select"
+                    value={this.state.availableToGig}
+                    onChange={this.handleChange}
+                    name="availableToGig"
+                  >
                     <option value=""></option>
                     <option value="Once a week">Once a week</option>
                     <option value="Twice a week">Twice a week</option>
@@ -322,7 +411,7 @@ class EditProfile extends Component {
                     onChange={this.handleInstrumentsTagChange}
                     value={this.state.instruments}
                     name="instruments"
-                    inputProps={{ placeholder: 'Add an Instrument' }}
+                    inputProps={{ placeholder: "Add an Instrument" }}
                   />
                 </div>
                 <div className="grid-full-width">
@@ -331,7 +420,7 @@ class EditProfile extends Component {
                     onChange={this.handleGenresTagChange}
                     value={this.state.genres}
                     name="genres"
-                    inputProps={{ placeholder: 'Add a Genre' }}
+                    inputProps={{ placeholder: "Add a Genre" }}
                   />
                 </div>
                 <div className="grid-full-width">
@@ -370,19 +459,29 @@ class EditProfile extends Component {
                     name="lookingForBandmates"
                     className="checkbox"
                   />
-                  <label className="checkbox-label">Looking For Bandmates</label>
+                  <label className="checkbox-label">
+                    Looking For Bandmates
+                  </label>
                 </div>
                 <div className="grid-full-width">
-                  <button type="button" className="button button-blue" onClick={this.toggleSocialInputs}>Edit social links</button>
+                  <button
+                    type="button"
+                    className="button button-blue"
+                    onClick={this.toggleSocialInputs}
+                  >
+                    Edit social links
+                  </button>
                 </div>
-                { displaySocialInputs && this.renderSocialInputs() }
+                {displaySocialInputs && this.renderSocialInputs()}
               </div>
-              <button className="button button-darkblue" type="submit">Confirm</button>
+              <button className="button button-darkblue" type="submit">
+                Confirm
+              </button>
             </form>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -390,13 +489,14 @@ EditProfile.propTypes = {
   profile: PropTypes.object.isRequired,
   errors: PropTypes.object.isRequired,
   createProfile: PropTypes.func.isRequired,
-  getCurrentProfile: PropTypes.func.isRequired
-}
+  getCurrentProfile: PropTypes.func.isRequired,
+};
 
 const mapStateToProps = (state) => ({
   profile: state.profile,
-  errors: state.errors
-})
+  errors: state.errors,
+});
 
-export default connect(mapStateToProps, { createProfile, getCurrentProfile })(withRouter(EditProfile))
-
+export default connect(mapStateToProps, { createProfile, getCurrentProfile })(
+  withRouter(EditProfile)
+);
